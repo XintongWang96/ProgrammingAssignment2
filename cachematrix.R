7167a86caae7eab9ec383ef92aab24a94b9876d9
XintongWang96 <34191042+XintongWang96@users.noreply.github.com>
2018-03-18 13:13:36 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
 
+### the first function is to make a matrix which contains the set, get, setinvr and getinvr
+### functions for calculation 
 makeCacheMatrix <- function(x = matrix()) {
+        m <- NULL
+        set <- function (y) {
+                x <<- y
+                m <<- NULL
+                }
+        get <- function() x
+        
+        setinvr <- fucntion (invr) m <- invr
+        getinvr <-function() m
+        list = ( set = set, get = get, setinvr = setinvr, getinvr = getinvr )
 
 }
 
 
-## Write a short comment describing this function
 
+### computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+### If the inverse has already been calculated,
+### then retrieve the inverse from the cache
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getinvr
+        if (! is.null(m)){ 
+                messgae(" Cache matrix ")
+                return ( m )
+                } 
+        matrix_original <- x$get
+        matrix_invr <- solve(matrix_original)
+        x$setinvr(matrix_invr)
+        matrix_invr
+        
 }
